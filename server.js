991346d2c264d2cb79228289a199dd3840785caa
sleem <sleem@localhost.localdomain>
2021-02-13 14:17:25 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const port = 3002
 
 const mockUserData=[
@@ -21,6 +23,27 @@ app.get('/users/:id', (req, res) => {console.log(req.params.id)
        message: 'got one user',
        user: req.params.id
    }) 
+})
+
+app.post('/login', (req, res) =>{
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="sleemTheKing";
+    const mockPassword="keepShut";
+
+    if (username===mockUsername && password===mockPassword){
+    res.json({
+        success: true,
+        message: 'password and username match!',
+        token: 'encrypted token goes here'
+    })
+    } else {
+    res.json({
+        success: false,
+        message: 'password and username do not match'
+    })
+    }
 })
 
 app.listen(port, () => console.log(`express app listening on port port! 3002`))
